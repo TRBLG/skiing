f2a8f6696a39a9090b3d42e75e4f7ab08255144e
TRBLG <Stone91trblg@gmail.com>
2017-06-25 14:04:06 +0700

    Реализовал полную генирацию карты.

--- TechnicalTask/skiing/Assets/Scripts/MapGeneration.cs
+++ TechnicalTask/skiing/Assets/Scripts/MapGeneration.cs
@@ -7,5 +7,59 @@ public class MapGeneration : MonoBehaviour {
     public GameObject stellar;
     Vector2[] points;
     Vector2 startPoint;
-    
+    int countPoints = 100; // колличество точек
+    float countPi = 2;
+    float downStep = 0.7f; //коофицент, на который уменьшаем y при создании каждой точки
+                            //чем больше это число, тем более отвесным будет склон и тем меньше будут возвышения
+    const float stepPi = 0.3f;
+    int amplitude = 2; //чем больше число, тем больше перепады высот
+
+    private void Start()
+    {
+        startPoint = collider.points[0];
+        
+        startGeneration();
+    }
+
+
+    private void FixedUpdate()
+    {
+        checkDistanse();
+    }
+
+    private void startGeneration()
+    {
+        points = new Vector2[countPoints];
+        points[0] = startPoint;
+
+        //проходимся по всему массиву создавая точки
+        for (int i = 1; i < countPoints; i++) {
+            addPoint(i);
+        }
+
+        collider.points = points;//задаем полученный массив векторов колайдеру
+    }
+
+    private void addPoint(int position)
+    {
+        points[position] = new Vector2(points[position - 1].x + 0.5f, points[position - 1].y - downStep 
+                                                    + (Mathf.Sin(countPi) * Random.value * amplitude));
+
+        countPi += stepPi;//увеличиваем Пи
+        if (countPi > Mathf.PI * 2) { countPi -= Mathf.PI * 2; }// обнуляем для измежание переполнения
+    }
+
+    private void checkDistanse()
+    {
+        if (stellar.transform.position.x - startPoint.x > 10) //проверяем расстоение от неваляшки до первой токи карты
+        {
+            startPoint = points[1];
+            for (int i = 1; i < points.Length; i++ ) //смещаем массив точек на 1 затирая первую
+            {
+                points[i - 1] = points[i];
+            }
+            addPoint(points.Length - 1); //добавляем точку в конец массива
+            collider.points = points;//задаем полученный массив векторов колайдеру
+        }
+    }
 }
