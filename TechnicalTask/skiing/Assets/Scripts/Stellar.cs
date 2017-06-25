e348397174c932f1f95f1b07ce078dccfe250a19
TRBLG <Stone91trblg@gmail.com>
2017-06-25 12:54:17 +0700

    Добавил неваляшке "пинки"

--- TechnicalTask/skiing/Assets/Scripts/Stellar.cs
+++ TechnicalTask/skiing/Assets/Scripts/Stellar.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class Stellar : MonoBehaviour {
     Transform centerOfMass;
     Rigidbody2D rb;
+    Vector3 vect;
+    public float speed = 250; // множитель импульса
+    
 
     void Awake()
     {
         centerOfMass = transform.Find("center"); //получаем центр масс
         rb = GetComponent<Rigidbody2D>(); //получаем Rigidbody2D
         rb.centerOfMass = Vector3.down; //смещаем центр масс для полученного rigidbody2D вниз
-
-
-
+        vect = new Vector3(0.5f, 0.3f, 0);// вектор, определяющий направление импульса
     }
-
 
     void Start () {
 		
 	}
-	
 
-	void Update () {
-		
-	}
+    private void OnMouseDown()
+    {
+        rb.AddForce(vect * speed);  //задаем неваляшке импульс при клике по ней
+    }
 }
